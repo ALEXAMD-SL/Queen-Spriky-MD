8fea46346c5f5ff42fe1850936dd076be9280a59
ALEXAMD-SL <malakaheroku@gmail.com>
2025-07-28 23:32:46 +0530

    Update config.js

--- config.js
+++ config.js
@@ -6,7 +6,7 @@ function convertToBool(text, fault = 'true') {
 }
 
 module.exports = {
-    SESSION_ID: process.env.SESSION_ID || '', // Enter Your Session ID
-    MONGODB: process.env.MONGODB || '',// Enter Your MongoDB URL
-    OWNER_NUMBER: process.env.OWNER_NUMBER || '',    // Enter Your Owner Number
+    SESSION_ID: process.env.SESSION_ID || 'RUJH0KBT#VB0uZiTindEwqe57RNOT23IQJIsoHZwO9_g3V8LovI4', // Enter Your Session ID
+    MONGODB: process.env.MONGODB || 'mongodb://mongo:TmuvvPxGsJKQDODUiJpQfADqsMPFsLIJ@switchback.proxy.rlwy.net:55923',// Enter Your MongoDB URL
+    OWNER_NUMBER: process.env.OWNER_NUMBER || '94742287793',    // Enter Your Owner Number
 };
